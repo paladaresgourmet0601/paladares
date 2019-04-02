d546f30ba6c4dade09c220863337f4142e4bf14e
paladaresgourmet0601 <49178899+paladaresgourmet0601@users.noreply.github.com>
2019-04-01 23:10:04 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>BIENVENIDA</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos/custom/estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script:700" rel="stylesheet">
+    </head>
+  <body id="fondo_back">
+    
+
+    <div class="container" style="margin-top: 11.5%;">
+        <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6 fondo_parrafo border-success rounded row" >
+                <p class="parrafo">El amor es tan importante como la comida pero !!no alimenta¡¡</p>
+                <div class="col-sm-2"></div>
+                  <div class="col-sm-8">
+                  <div class="row">
+                    <div class="col">
+                      <a class="btn btn-outline-info my-2 my-sm-0 " href="pages/es-es/login.php">Iniciar Sesion</a>
+                    </div>
+                      <div class="col">
+                        <a class="nav-item dropdown dropdown-toggle btn btn-outline-success" href="main.php" id="navbarDropdown" role="button" data-toggle="dropdown">
+                        Registrarse Como</a>
+                        <div class="dropdown-menu" style="text-align: center;" aria-labelledby="navbarDropdown">
+                          <a class="btn btn-outline-success my-2 my-sm-0 " href="pages/es-es/regrestaurantes.php">Restaurante</a>
+                          <p></p>
+                          <a class="btn btn-outline-success my-2 my-sm-0 " href="pages/es-es/regiscomensal.php">Comensales</a>
+                        </div>  
+                        <p></p>
+                      </div>
+                    </div>
+                  </div>
+                <div class="col-sm-2"></div>
+            </div>
+            <div class="col-sm-3">
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br> <br>
+    <?php
+      include "config/footer1.php";
+    ?>  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
